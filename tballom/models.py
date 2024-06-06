c093458cc8dd494fa8b01b197404ff7a6861d40c
boram065 <boram4784@gmail.com>
2024-06-06 21:59:32 +0900

    Feat: bat 테이블에 배트이름 및 가격 초기 값으로 insert

--- tballom/models.py
+++ tballom/models.py
@@ -2,31 +2,22 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=10, null=False)
-    password = models.CharField(max_length=10, null=False)
-    createAt = models.DateTimeField(auto_now_add=True)
-    updateAt = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.user_name} : {self.password}'
+    user_name = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Point(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     user_point = models.IntegerField()
 
-    def __str__(self):
-        return f'{self.user.user_name} : {self.user_point}'
-
 class Score(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     user_score = models.IntegerField()
-
-    def __str__(self):
-        return f'{self.user.user_name} : {self.user_score}'
 
 class Bat(models.Model):
     bat_name = models.CharField(max_length=20)
     bat_price = models.IntegerField()
 
-    def __str__(self):
-        return self.bat_name
+class UserBat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bat = models.ForeignKey(Bat, on_delete=models.CASCADE)
