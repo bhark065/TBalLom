f52aa71a54afec053d85bb375c090f7c5edfcdb7
boram065 <boram4784@gmail.com>
2024-06-07 15:47:27 +0900

    Feat: 스윙 기능 추가

--- tballom/urls.py
+++ tballom/urls.py
@@ -5,7 +5,5 @@ app_name = "tballom"
 
 urlpatterns = [
     path('', views.tballom_name_view, name='tballom_name_view'),
-    # path('', views.tballom_name_view, name='Main'),
-    # path('game/', views.tballom_game_view, name='Game'),
     path('game/<int:pk>/', views.tballom_game_view, name='tballom_game'),
 ]
