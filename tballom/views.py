8de8d838becd9145a356bf0136f202ad4f5bd543
rkdbs <mw2207@naver.com>
2024-06-27 14:45:17 +0900

    Feat: 사용자 이름 보여주기

--- tballom/views.py
+++ tballom/views.py
@@ -6,31 +6,23 @@ from .models import User, Point, Score
 from django.http import JsonResponse
 
 
-# Create your views here.
-
 def tballom_name_view(request):
     user_name = request.POST.get('user_name')
     if not user_name:
         return render(request, 'html/tballom/tballom_name.html')
 
-    # user_name이 이미 존재하는지 확인
     user = User.objects.filter(user_name=user_name).first()
-
     if user:
-        # 이미 존재하는 사용자 이름일 경우
         return redirect('tballom:tballom_game_view')
     else:
-        # 새로운 사용자 이름일 경우
         new_user = User.objects.create(user_name=user_name)
         return redirect('tballom:tballom_game_view')
 
 def tballom_game_view(request):
-    user = get_object_or_404(User)
-    user_id = request.user.id
-    user_point = Point.objects.filter(user_id=user_id).first()
+    user = get_object_or_404(User, id=request.user.id)
+    user_point = Point.objects.filter(user=user).first()
     return render(request, 'html/tballom/tballom_game.html', {'user': user, 'user_point': user_point})
 
-# score 저장
 @csrf_exempt
 def save_score(request):
     if request.method == 'POST':
@@ -40,15 +32,12 @@ def save_score(request):
             user_score = data.get('user_score')
 
             user = User.objects.get(id=user_id)
-
             score, created = Score.objects.get_or_create(user=user)
             if not created:
-                # 기존 점수가 있을 경우 비교하여 더 높은 점수로 업데이트
                 if score.user_score < user_score:
                     score.user_score = user_score
                     score.save()
             else:
-                # 기존 점수가 없을 경우 새로 저장
                 score.user_score = user_score
                 score.save()
 
@@ -71,7 +60,6 @@ def save_point(request):
             user_score = data.get('user_score')
 
             user = User.objects.get(id=user_id)
-
             point, created = Point.objects.get_or_create(user=user, defaults={'user_point': 0})
             point.user_point += user_score
             point.save()
@@ -90,4 +78,5 @@ def tballom_store_view(request):
     return render(request, 'html/tballom/tballom_store.html')
 
 def tballom_rank_view(request):
-    return render(request, 'html/tballom/tballom_rank.html')
+    users = User.objects.all()
+    return render(request, 'html/tballom/tballom_rank.html', {'users': users})
